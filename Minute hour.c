0e229f795763bb5a84b9e5f2c18301fa8a20a49b
Lewis-8-44 <aakarsingh84@gmail.com>
2025-08-21 23:34:46 +0530

    Add files via upload

---
+++ Minute hour.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+ int main ()  {
+
+     int minutes,hours;
+     printf("Enter Number of Hours");
+     scanf("%d",&hours);
+
+     minutes = hours*60;
+
+    printf("%d hours = %d minutes",hours,minutes);
+
+    return 0;
+
+ }
+
+
