837a3569b32fbfa4d9aba09b08420201860e8ea8
Lewis-8-44 <aakarsingh84@gmail.com>
2025-08-22 01:17:45 +0530

    Add files via upload

---
+++ Area of circle.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+ int main ()  {
+
+     int r,a;
+     printf("Enter radius of circle");
+     scanf("%d",&r);
+
+    a=22/7*r*r;
+
+
+     printf("the circle of radius %d m has area %d sq m ",r,a);
+
+    return 0;
+
+ }
---
+++ Area of triangle.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+ int main ()  {
+
+     int h,l,a;
+     printf("Enter height and legth of base of triangle");
+     scanf("%d",&l);
+
+    a = h*l/2;
+
+
+     printf("the triangle of height %d m and lenght of base %d m has area %d sq m ",l,h,a);
+
+    return 0;
+
+ }
