57aadd01f199ccbf9bd7b7710675c022c2dfcb19
Lewis-8-44 <aakarsingh84@gmail.com>
2025-08-21 22:46:07 +0530

    Add files via upload

---
+++ Diff.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+
+    int a,b,c;
+    printf("Enter value of a and b");
+    scanf("%d%d",&a,&b);
+    printf("The Difference of %d and %d is %d",a,b,(a-b));
+
+    return 0;
+
+}
---
+++ Product.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+
+    int a,b;
+    printf("Enter value of a and b");
+    scanf("%d%d",&a,&b);
+    printf("The product of %d and %d is %d",a,b,(a*b));
+
+    return 0;
+
+}
+
