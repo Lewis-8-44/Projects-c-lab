c67de748e8e252db312f2ac6c6cc2395606d4734
Lewis-8-44 <aakarsingh84@gmail.com>
2025-08-21 22:46:51 +0530

    Add files via upload

---
+++ sum1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+
+    int a,b,c;
+    printf("Enter value of a and b");
+    scanf("%d%d",&a,&b);
+    printf("The Sum of %d and %d is %d",a,b,(a+b));
+
+    return 0;
+
+}
+
