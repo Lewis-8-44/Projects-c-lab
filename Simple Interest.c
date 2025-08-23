c89dd48c12ef52e7734895b36773a3dcf19c876b
Lewis-8-44 <aakarsingh84@gmail.com>
2025-08-23 16:36:16 +0530

    Add files via upload

---
+++ Simple Interest.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+    void main() {
+    float i,p,r,n;
+    printf("Enter principle amount,rate of interest and time",p,r,n);
+    scanf("%f%f%f",&p,&r,&n);
+    i=p*r*n/100;
+    printf("The Simple Interest is %f",i);
+    }
