821aa272115c095ef30cff5a5c543349ed7476e0
Lewis-8-44 <aakarsingh84@gmail.com>
2025-08-27 16:03:06 +0530

    Add files via upload

---
+++ Rupees to dollars.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+ void main ()  {
+
+     float rupees,dollars;
+     printf("Enter Amount in Rupees\n");
+     scanf("%f",&rupees);
+
+    dollars  = rupees/48;
+
+    printf("%f rupees  = %f dollars",rupees,dollars);
+
+
+ }
+
