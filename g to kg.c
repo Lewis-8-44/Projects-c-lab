7d6b6af33d4d08842849ec6eac68da2dd2ac3fc2
Lewis-8-44 <aakarsingh84@gmail.com>
2025-08-27 15:56:09 +0530

    Add files via upload

---
+++ g to kg.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+ void main ()  {
+
+     float g,kg;
+     printf("Enter Amount in Grams\n");
+     scanf("%f",&g);
+
+      kg = g/1000;
+    printf("%f grams = %f kilograms",g,kg);
+
+ }
---
+++ kg to g.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+ int main ()  {
+
+     int g,kg;
+     printf("Enter Amount in kilograms\n");
+     scanf("%d",&kg);
+     g=kg*1000;
+    printf("%d kilograms = %d grams",kg,g);
+
+
+ }
