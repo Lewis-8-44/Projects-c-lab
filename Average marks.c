e1ca737205f4dc760d966a5e23c79500504310cd
Lewis-8-44 <aakarsingh84@gmail.com>
2025-08-27 15:25:18 +0530

    Add files via upload

---
+++ Average marks.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+   float main () {
+   float p,c,m,a,t;
+   printf("Enter of Marks of Physics , Chemistry and Maths");
+   scanf("%f%f%f",&p,&c,&m);
+   a=(p+c+m)*1/3;
+   t=p+c+m;
+   printf("The Average Marks of Physics , Chemistry and Maths is %f and total is %f",a,t);
+
+   }
+
