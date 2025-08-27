114672f6ac9f63c38254e05eb784ba524d9b3216
Lewis-8-44 <aakarsingh84@gmail.com>
2025-08-27 15:31:21 +0530

    Add files via upload

---
+++ swap 2 values.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+   void main()
+{
+   int a,b;
+   printf("Enter value of a\n");
+   scanf("%d",&a);
+   printf("Enter value of b\n");
+   scanf("%d",&b);
+   printf("Value of a is %d\n",b);
+   printf("Value of b is %d\n",a);
+}
+
